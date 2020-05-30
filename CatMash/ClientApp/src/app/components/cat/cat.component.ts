395e6f09e2ec7a3504f686a6b610aea9bb00c22a
hamdi <hamdi.chehdi@outlook.com>
2020-05-30 08:11:30 +0200

    change the detection strategy for cat component (because it is a presentation component) to OnPush strategy

--- CatMash/ClientApp/src/app/components/cat/cat.component.ts
+++ CatMash/ClientApp/src/app/components/cat/cat.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, ChangeDetectionStrategy } from '@angular/core';
 import { Cat } from '../../models/Cat';
 
 @Component({
   selector: 'app-cat',
   templateUrl: './cat.component.html',
-  styleUrls: ['./cat.component.css']
+  styleUrls: ['./cat.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CatComponent {
 
